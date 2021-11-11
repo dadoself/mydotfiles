10f5c68bf7f2f159ec23064b1dbd8818fb21a8ee
Davide Bianco <dadoself@gmail.com>
2021-11-11 13:04:34 +0100

    Doom Emacs is fantastic!

---
+++ .doom.d/config.el
@@ -0,0 +1,12 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Davide Bianco"
+      user-mail-address "dadoself@gmail.com")
+
+(setq doom-theme 'doom-one)
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type t)
+
+(setq lsp-rust-analyzer-proc-macro-enable t)
